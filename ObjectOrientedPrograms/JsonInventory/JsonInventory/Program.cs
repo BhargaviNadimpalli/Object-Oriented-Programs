0b51aa872ad8609856b417f750b16c894fdece99
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-20 17:34:16 -0600

    Object oriented programs

---
+++ ObjectOrientedPrograms/JsonInventory/JsonInventory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JsonInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to object oriented programs");
+            InventorDetails obj = new InventorDetails();
+        }
+    }
+}
---
+++ ObjectOrientedPrograms/StockManagement/StockManagement/Stock.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagement
+{
+    class Stock
+    {
+        public int totalshare = 0;
+        public int totalvalue = 0;
+        public void Stockvalue(int a, int b, string c)
+        {
+            int stockvalue = a * b;
+            Console.WriteLine("the value of the stock for" + c + " shares is :  " + stockvalue);
+            totalvalue += (a * b);
+            Console.WriteLine("the total value of the stock is  " + totalvalue);
+
+
+        }
+    }
+}
